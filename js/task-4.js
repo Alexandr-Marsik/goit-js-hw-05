a76640387f0e5071aaf33efbbb2ce0528bad2671
Alexandr <lesiv.sania@gmail.com>
2025-01-22 16:52:50 +0200

    1

--- js/task-4.js
+++ js/task-4.js
@@ -3,9 +3,7 @@
 const getTotalBalanceByGender = (users, gender) =>
   users
     .filter(user => user.gender === gender)
-    .reduce((acc, user) => {
-      return acc + user.balance;
-    }, 0);
+    .reduce((acc, user) => acc + user.balance, 0);
 const clients = [
   {
     name: 'Moore Hensley',
